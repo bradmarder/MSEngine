7a3aa865832d80a2b964c804127e925e4f630e18
bradmarder <>
2020-06-07 09:57:06 -0400

    benchmark cleanup

--- src/MSEngine.Benchmarks/Program.cs
+++ src/MSEngine.Benchmarks/Program.cs
@@ -108,7 +108,7 @@ namespace MSEngine.Benchmarks
                     if (turnCount == 0)
                     {
                         turnCount = MatrixSolver.CalculateTurns(matrix, turns, true);
-						if (turnCount == 0){ break; }
+                        if (turnCount == 0) { break; }
                     }
                     foreach (var turn in turns.Slice(0, turnCount))
                     {
@@ -118,39 +118,5 @@ namespace MSEngine.Benchmarks
                 iteration++;
             }
         }
-    }
-
-    public static class Util
-    {
-        public static void FillAdjacentNodeIndexesNEW(this Span<int> indexes, int nodeCount, int index, int columnCount)
-        {
-            var key = GetKey(nodeCount, index, columnCount);
-            _keyToAdjacentNodeIndexMap![key].CopyTo(indexes);
-        }
-
-        private static IReadOnlyDictionary<uint, int[]>? _keyToAdjacentNodeIndexMap;
-
-        static Util()
-        {
-            var map = new Dictionary<uint, int[]>(64 + 256 + 480); // beginner/int/expert
-
-            Span<int> buffer = stackalloc int[8];
-            for (var i = 0; i < 64; i++)
-            {
-                var key = GetKey(64, i, 8);
-                buffer.FillAdjacentNodeIndexes(64, i, 8);
-                map.Add(key, buffer.ToArray());
-            }
-
-            _keyToAdjacentNodeIndexMap = map;
-        }
-
-        /// <summary>
-        /// 4 byte key
-        /// 8 bits for columnCount, 12/12 for index/nodeCount
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static uint GetKey(int nodeCount, int index, int columnCount)
-            => (uint)columnCount | ((uint)nodeCount << 8) | ((uint)index << 20);
     }
 }
